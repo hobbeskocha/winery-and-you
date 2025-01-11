5c096cf262e90ca926b5ee399333fc0b17a0807c
hobbeskocha <ayu.yoshi.shres@gmail.com>
2025-01-11 14:36:11 -0800

    version control py to ipynb script

---
+++ vc-py-to-ipynb.sh
@@ -0,0 +1,3 @@
+jupytext --to .ipynb --output src/1_data_cleaning.ipynb src/1_data_cleaning.py
+jupytext --to .ipynb --output src/2_descriptive_analysis.ipynb src/2_descriptive_analysis.py
+jupytext --to .ipynb --output src/3_predictive_analysis.ipynb src/3_predictive_analysis.py
