da2bb487d9d3958900a9eedd1750241b85e38a96
hobbeskocha <ayu.yoshi.shres@gmail.com>
2025-01-12 15:52:45 -0800

    be fe v1 setup for 3 logit predictions

--- src/app/app.py
+++ src/app/app.py
@@ -1,24 +1,31 @@
 import streamlit as st
 import requests
 
-API_URL = "http://127.0.0.1:8000/predict"
+EMAIL_API_URL = "http://127.0.0.1:8000/predict-email"
+NEWSLETTER_API_URL = "http://127.0.0.1:8000/predict-newsletter"
+WINEMAKER_API_URL = "http://127.0.0.1:8000/predict-winemaker"
 
+
+# Header
 st.title = "Winery Subscription Predictions"
-st.write("This is a simple app to predict whether a customer will subscribe to the winery email.")
+st.write("This is a simple app to predict whether a customer will subscribe to the winery's emails, newsletters, or winemaker calls.")
 
 # User Inputs
-order_volume = st.number_input("Order Volume", min_value=0, max_value=100_000, value=5)
-customer_segment = st.selectbox("Customer Segment", ["High Roller", "Luxury Estate",
-                                                      "Wine Enthusiast", "Casual Visitor"])
-division = st.selectbox("Division", ["New England", "Middle Atlantic", "East North Central",
-                                      "West North Central", "South Atlantic", "East South Central",
-                                      "West South Central", "Mountain", "Pacific"])
-sale_amount = st.number_input("Sale Amount", min_value=0.0, max_value=100_000.0, value=5)
-newsletter_subscr = st.checkbox("Newsletter Subscription")
-winemaker_call_subscr = st.checkbox("Winemaker Call Subscription")
 
-# Make predictions
-if st.button("Predict"):
+## Email Subscription
+st.write("Email Subscription")
+sale_amount = st.number_input("Total Sale Amount", min_value=0.0, max_value=100_000.0, value=5.0, key="email_sale_amount")
+order_volume = st.number_input("Order Volume", min_value=0, max_value=100_000, value=5, key="email_order_volume")
+customer_segment = st.selectbox("Customer Segment", ["High Roller", "Luxury Estate",
+                                                      "Wine Enthusiast", "Casual Visitor"], key="email_customer_segment")
+division = st.selectbox("US Division", ["New England", "Middle Atlantic", "East North Central",
+                                      "West North Central", "South Atlantic", "East South Central",
+                                      "West South Central", "Mountain", "Pacific"], key="email_division")
+newsletter_subscr = st.checkbox("Newsletter Subscription", key="email_newsletter_subscr")
+winemaker_call_subscr = st.checkbox("Winemaker Call Subscription", key="email_winemaker_subscr")
+
+## Email Predictions
+if st.button("Predict Email Subscription"):
     input = {
         "OrderVolume": order_volume,
         "CustomerSegment": customer_segment,
@@ -28,11 +35,74 @@ if st.button("Predict"):
         "WinemakerCallSubscr": winemaker_call_subscr
     }
 
-    response = requests.post(API_URL, json=input)
+    response = requests.post(EMAIL_API_URL, json=input)
 
     if response.status_code == 200:
-        prediction = response.json(["prediction"])
-        st.success(f"Prediction: {prediction}")
+        prediction = response.json()["prediction"]
+        st.success(f"Email Prediction: {prediction}", icon="🎉")
     else:
-        st.error("Error: Unable to make prediction")
+        st.error("Error: Unable to make Email prediction")
 
+
+## Newsletter Subscription
+st.write("Newsletter Subscription")
+sale_amount = st.number_input("Total Sale Amount", min_value=0.0, max_value=100_000.0, value=5.0, key="newsletter_sale_amount")
+order_volume = st.number_input("Order Volume", min_value=0, max_value=100_000, value=5, key="newsletter_order_volume")
+customer_segment = st.selectbox("Customer Segment", ["High Roller", "Luxury Estate",
+                                                      "Wine Enthusiast", "Casual Visitor"], key="newsletter_customer_segment")
+division = st.selectbox("US Division", ["New England", "Middle Atlantic", "East North Central",
+                                      "West North Central", "South Atlantic", "East South Central",
+                                      "West South Central", "Mountain", "Pacific"], key="newsletter_division")
+email_subscr = st.checkbox("Email Subscription", key = "newsletter_email_subscr")
+winemaker_call_subscr = st.checkbox("Winemaker Call Subscription", key="newsletter_winemaker_subscr")
+
+## Newsletter Predictions
+if st.button("Predict Newsletter Subscription"):
+    input = {
+        "OrderVolume": order_volume,
+        "CustomerSegment": customer_segment,
+        "Division": division,
+        "SaleAmount": sale_amount,
+        "WinemakerCallSubscr": winemaker_call_subscr,
+        "EmailSubscr": email_subscr
+    }
+
+    response = requests.post(NEWSLETTER_API_URL, json=input)
+
+    if response.status_code == 200:
+        prediction = response.json()["prediction"]
+        st.success(f"Newsletter Prediction: {prediction}", icon="🎉")
+    else:
+        st.error("Error: Unable to make Newsletter prediction")
+
+
+## Winemaker Call Subscription
+st.write("Winemaker Call Subscription")
+sale_amount = st.number_input("Total Sale Amount", min_value=0.0, max_value=100_000.0, value=5.0, key="winemaker_sale_amount")
+order_volume = st.number_input("Order Volume", min_value=0, max_value=100_000, value=5, key="winemaker_order_volume")
+customer_segment = st.selectbox("Customer Segment", ["High Roller", "Luxury Estate",
+                                                      "Wine Enthusiast", "Casual Visitor"], key="winemaker_customer_segment")
+division = st.selectbox("US Division", ["New England", "Middle Atlantic", "East North Central",
+                                      "West North Central", "South Atlantic", "East South Central",
+                                      "West South Central", "Mountain", "Pacific"], key="winemaker_division")
+email_subscr = st.checkbox("Email Subscription", key="winemaker_email_subscr")
+newsletter_subscr = st.checkbox("Newsletter Subscription", key="winemaker_newsletter_subscr")
+
+## Winemaker Predictions
+if st.button("Predict Winemaker Subscription"):
+    input = {
+        "OrderVolume": order_volume,
+        "CustomerSegment": customer_segment,
+        "Division": division,
+        "SaleAmount": sale_amount,
+        "NewsletterSubscr": email_subscr,
+        "EmailSubscr": winemaker_call_subscr
+    }
+
+    response = requests.post(WINEMAKER_API_URL, json=input)
+
+    if response.status_code == 200:
+        prediction = response.json()["prediction"]
+        st.success(f"Winemaker Prediction: {prediction}", icon="🎉")
+    else:
+        st.error("Error: Unable to make Winemaker prediction")
--- src/app/backend.py
+++ src/app/backend.py
@@ -1,26 +1,117 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 import joblib
+import logging
 
 model_log_email = joblib.load("../model-artifacts/log_email.pkl")
+model_log_newsletter = joblib.load("../model-artifacts/log_newsletter.pkl")
+model_log_winemaker = joblib.load("../model-artifacts/log_winemaker.pkl")
 
 backend_app = FastAPI()
+logger = logging.getLogger()
+logging.basicConfig(level=logging.INFO)
 
 class InputData(BaseModel):
     OrderVolume: int
-    CustomerSegment: str # TODO: one-hot encode
-    Division: str # TODO: one-hot encode
+    CustomerSegment: str 
+    Division: str 
     SaleAmount: float
+
+class EmailInputData(InputData):
     NewsletterSubscr: bool 
     WinemakerCallSubscr: bool 
 
-@backend_app.post("/predict")
-def predict(data: InputData):
-    input_data = [[data.OrderVolume, data.CustomerSegment,
-                    data.Division, data.SaleAmount,
-                      data.NewsletterSubscr, data.WinemakerCallSubscr]] # TODO: one-hot encode categorical variables
+class WinemakerInputData(InputData):
+    NewsletterSubscr: bool
+    EmailSubscr: bool
+
+class NewsletterInputData(InputData):
+    WinemakerCallSubscr: bool 
+    EmailSubscr: bool
+
+def encode_customer_segment(segment: str):
+    is_high_roller = 1 if segment == "High Roller" else 0
+    is_luxury_estate = 1 if segment == "Luxury Estate" else 0
+    is_wine_enthusiast = 1 if segment == "Wine Enthusiast" else 0
+    return [is_high_roller, is_luxury_estate, is_wine_enthusiast]
+
+def encode_division(division: str):
+    is_east_south_central = 1 if division == "East South Central" else 0
+    is_middle_atlantic = 1 if division == "Middle Atlantic" else 0
+    is_mountain = 1 if division == "Mountain" else 0
+    is_new_england = 1 if division == "New England" else 0
+    is_pacific = 1 if division == "Pacific" else 0
+    is_south_atlantic = 1 if division == "South Atlantic" else 0
+    is_west_north_central = 1 if division == "West North Central" else 0
+    is_west_south_central = 1 if division == "West South Central" else 0
+
+    return [is_east_south_central, is_middle_atlantic, is_mountain,
+             is_new_england, is_pacific, is_south_atlantic,
+               is_west_north_central, is_west_south_central]
+
+
+@backend_app.post("/predict-email")
+def predict_email(data: EmailInputData):
+    # map categorical data for one-hot encoding
+    is_high_roller, is_luxury_estate, is_wine_enthusiast = encode_customer_segment(data.CustomerSegment)
+    (is_east_south_central, is_middle_atlantic, is_mountain,
+      is_new_england, is_pacific, is_south_atlantic,
+        is_west_north_central, is_west_south_central) = encode_division(data.Division)
+
+    # config input data for model prediction
+    input_data = [[1.0, data.OrderVolume, data.SaleAmount, 
+                   is_high_roller, is_luxury_estate, is_wine_enthusiast, 
+                   is_east_south_central, is_middle_atlantic, is_mountain,
+                   is_new_england, is_pacific, is_south_atlantic,
+                    is_west_north_central, is_west_south_central,
+                    data.NewsletterSubscr, data.WinemakerCallSubscr]]
+    
     prediction = model_log_email.predict(input_data)
-    return {"prediction": int(prediction[0])}
+    logging.info(f"Email Probability: {prediction} and Prediction: {round(prediction[0])}")
+    return {"prediction": round(prediction[0])}
+
+
+@backend_app.post("/predict-newsletter")
+def predict_newsletter(data: NewsletterInputData):
+    # map categorical data for one-hot encoding
+    is_high_roller, is_luxury_estate, is_wine_enthusiast = encode_customer_segment(data.CustomerSegment)
+    (is_east_south_central, is_middle_atlantic, is_mountain,
+      is_new_england, is_pacific, is_south_atlantic,
+        is_west_north_central, is_west_south_central) = encode_division(data.Division)
+    
+    # config input data for model prediction
+    input_data = [[1.0, data.OrderVolume, data.SaleAmount, 
+                   is_high_roller, is_luxury_estate, is_wine_enthusiast, 
+                   is_east_south_central, is_middle_atlantic, is_mountain,
+                   is_new_england, is_pacific, is_south_atlantic,
+                    is_west_north_central, is_west_south_central,
+                    data.WinemakerCallSubscr, data.EmailSubscr]]
+    
+    prediction = model_log_newsletter.predict(input_data)
+    logging.info(f"Newsletter Probability: {prediction} and Prediction: {round(prediction[0])}")
+    return {"prediction": round(prediction[0])}
+
+
+@backend_app.post("/predict-winemaker")
+def predict_winemaker(data: WinemakerInputData):
+    # map categorical data for one-hot encoding
+    is_high_roller, is_luxury_estate, is_wine_enthusiast = encode_customer_segment(data.CustomerSegment)
+    (is_east_south_central, is_middle_atlantic, is_mountain,
+      is_new_england, is_pacific, is_south_atlantic,
+        is_west_north_central, is_west_south_central) = encode_division(data.Division)
+    
+    # config input data for model prediction
+    input_data = [[1.0, data.OrderVolume, data.SaleAmount, 
+                   is_high_roller, is_luxury_estate, is_wine_enthusiast, 
+                   is_east_south_central, is_middle_atlantic, is_mountain,
+                   is_new_england, is_pacific, is_south_atlantic,
+                    is_west_north_central, is_west_south_central,
+                    data.NewsletterSubscr, data.EmailSubscr]]
+    
+    prediction = model_log_winemaker.predict(input_data)
+    logging.info(f"Winemaker Probability: {prediction} and Prediction: {round(prediction[0])}")
+    return {"prediction": round(prediction[0])}
+
 
 if __name__ == "__main__":
     import uvicorn
